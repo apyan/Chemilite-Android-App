23e8e81af71917022372ac76a99300e771c6499a
Alvin <apyan7852@gmail.com>
2017-10-01 03:27:20 -0700

    Layout Fix and Testing Setup
    Espresso Set-Up and Layout Change
    Still working on propering the layout.

---
+++ ChemPort/app/src/androidTest/java/com/spectrus/chemport/InstrumentedTest.java
@@ -0,0 +1,48 @@
+package com.spectrus.chemport;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class InstrumentedTest {
+
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.spectrus.chemport", appContext.getPackageName());
+    }
+
+    @Rule
+    public ActivityTestRule<SplashScreen> mTitleRule =
+            new ActivityTestRule(SplashScreen.class);
+
+    @Test
+    public void OpenUpApplicationTest() {
+        //locate and click on the login button
+        //onView(withId(R.id.button_sign_up)).perform(click());
+    }
+
+}
